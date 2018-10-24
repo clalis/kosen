ae143fe3084ceb4a078bd4704cece0fdeee26627
s_clalis <s_clalis@yahoo.co.jp>
2018-10-24 19:38:37 +0900

    Completed pp2_ex3
    やばいpp3終わんない。

--- Java/PracticalProgramming2/example02/src/pr2calc/SourceExample.java
+++ Java/PracticalProgramming2/example02/src/pr2calc/SourceExample.java
@@ -1,19 +1,20 @@
 package pr2calc;
 
-import javax.management.AttributeList;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SourceExample
 {
-    private int a;
-    private int[][] b;
-    private String str;
-
+//    private int a;
+//    private int[][] b;
+//    private String str;
+//
 //    public SourceExample()
 //    {
 //        int h, i;
@@ -25,142 +26,134 @@ public class SourceExample
 //
 //        this.str = "";
 //    }
-
+//
 //    public SourceExample(int a, int[][] b, String str)
 //    {
 //        this.a = a;
 //        this.b = b;
 //        this.str = str;
 //    }
-
-    public SourceExample()
-    {
-        try
-        {
-            saveData("savedSample");
-        }
-        catch (IOException e)
-        {
-            System.out.println("Failed to open files.");
-            e.printStackTrace();
-            System.exit(1);
-        }
-
-    }
-
-    public SourceExample(String fileName)
-    {
-        try
-        {
-            loadData(fileName);
-        }
-        catch (IOException e)
-        {
-            System.out.println("Failed to open files.");
-            System.exit(1);
-//            e.printStackTrace();
-        }
-    }
-
+//
+//
+//    public SourceExample(String fileName)
+//    {
+//        try
+//        {
+//            loadData(fileName);
+//        }
+//        catch (IOException e)
+//        {
+//            System.out.println("Failed to open files.");
+//            System.exit(1);
+////            e.printStackTrace();
+//        }
+//    }
+//
 //    public void setA(int value)
 //    {
 //        this.a = value;
 //    }
-
-    public int getA()
-    {
-        return this.a;
-    }
-
+//
+//    public int getA()
+//    {
+//        return this.a;
+//    }
+//
 //    public void setB(int[][] value)
 //    {
 //        this.b = value;
 //    }
-
+//
 //    public int[][] getB()
 //    {
 //        return this.b;
 //    }
-
+//
 //    public void setStr(String value)
 //    {
 //        this.str = value;
 //    }
+//
+//    public String getStr()
+//    {
+//        return this.str;
+//    }
+//
+//    public void showAllContentsOfB()
+//    {
+//        for (int[] aB : this.b)
+//        {
+//            for (int j = 0; j < aB.length; j++)
+//            {
+//                System.out.print(" " + aB[j]);
+//                if (j >= (aB.length - 1)) break;
+//                System.out.print(",");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//    private void loadData(String fileName) throws IOException
+//    {
+//        int row, column;
+//        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+//        String buf;
+//        String inputData;
+//        String[] inputValue;
+//
+//        inputData = reader.readLine();
+//        inputValue = inputData.split("\\s");
+//
+//        if (inputValue.length != 1) return;
+//        else
+//        {
+//            this.a = Integer.parseInt(inputValue[0]);
+//            inputData = reader.readLine();
+//            inputValue = inputData.split("\\s");
+//            if (inputValue.length != 2) return;
+//            else
+//            {
+//                row = Integer.parseInt(inputValue[0]);
+//                column = Integer.parseInt(inputValue[1]);
+//                this.b = new int[row][column];
+//                for (int h = 0; h < this.b.length; h++)
+//                {
+//                    inputData = reader.readLine();
+//                    inputValue = inputData.split("\\s");
+//                    for (int i = 0; i < this.b[h].length; i++) this.b[h][i] = Integer.parseInt(inputValue[i]);
+//                }
+//                inputData = reader.readLine();
+//                inputValue = inputData.split("\\s");
+//                buf = Arrays.stream(inputValue).map(anInputValue -> anInputValue + " ").collect(Collectors.joining());
+//                this.str = buf;
+//            }
+//        }
+//        reader.close();
+//    }
 
-    public String getStr()
+    public void saveData(String saveFileName) throws IOException
     {
-        return this.str;
-    }
-
-    public void showAllContentsOfB()
-    {
-        for (int[] aB : this.b)
-        {
-            for (int j = 0; j < aB.length; j++)
-            {
-                System.out.print(" " + aB[j]);
-                if (j >= (aB.length - 1)) break;
-                System.out.print(",");
-            }
-            System.out.println();
-        }
-    }
-
-    private void loadData(String fileName) throws IOException
-    {
-        int row, column;
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        String buf;
-        String inputData;
-        String[] inputValue;
-
-        inputData = reader.readLine();
-        inputValue = inputData.split("\\s");
-
-        if (inputValue.length != 1) return;
-        else
-        {
-            this.a = Integer.parseInt(inputValue[0]);
-            inputData = reader.readLine();
-            inputValue = inputData.split("\\s");
-            if (inputValue.length != 2) return;
-            else
-            {
-                row = Integer.parseInt(inputValue[0]);
-                column = Integer.parseInt(inputValue[1]);
-                this.b = new int[row][column];
-                for (int h = 0; h < this.b.length; h++)
-                {
-                    inputData = reader.readLine();
-                    inputValue = inputData.split("\\s");
-                    for (int i = 0; i < this.b[h].length; i++) this.b[h][i] = Integer.parseInt(inputValue[i]);
-                }
-                inputData = reader.readLine();
-                inputValue = inputData.split("\\s");
-                buf = Arrays.stream(inputValue).map(anInputValue -> anInputValue + " ").collect(Collectors.joining());
-                this.str = buf;
-            }
-        }
-        reader.close();
-    }
-
-    private void saveData(String saveFileName) throws IOException
-    {
-        int n = 5;
-        Integer[] sizeIntegers = {5, 5};
+        int n = 37;
+        Integer[] sizeIntegers = {3, 3};
         String s = "Hello world!";
         List<ArrayList<Object>> lists = new ArrayList<>();
         PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(saveFileName + ".dat")));
 
         lists.add(new ArrayList<>(Arrays.asList(n)));
         lists.add(new ArrayList<>(Arrays.asList(sizeIntegers)));
-        for (int i = 0; i < sizeIntegers[0]; i++) {
+        for (int i = 0; i < sizeIntegers[0]; i++)
+        {
             List<Object> objects = new ArrayList<>();
             for (int j = 0; j < sizeIntegers[1]; j++) objects.add(i * j);
             lists.add((ArrayList<Object>) objects);
         }
         lists.add(new ArrayList<>(Collections.singleton(s)));
-//        System.out.println(lists);
+
+        lists.forEach(objects ->
+        {
+            objects.stream().map(s1 -> String.valueOf(new StringBuilder().append(s1).append(" "))).forEachOrdered(writer::print);
+            writer.println();
+        });
 
         writer.close();
     }
