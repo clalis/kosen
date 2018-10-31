acce15462c22d3953d36f543e02e9abddfb76468
s_clalis <s_clalis@yahoo.co.jp>
2018-10-31 12:21:55 +0900

    Fixed pp3
    なおした、うごくうごく。

--- Java/PracticalProgramming2/example03/src/pr2calc/Matrix.java
+++ Java/PracticalProgramming2/example03/src/pr2calc/Matrix.java
@@ -3,46 +3,57 @@ package pr2calc;
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
 
-public class Matrix {
+public class Matrix
+{
     public int numRow, numCol;
     public RealMatrix m;
 
-    public Matrix() {
+    public Matrix()
+    {
         numRow = 0;
         numCol = 0;
     }
 
-    public Matrix(double[][] input) {
+    public Matrix(double[][] input)
+    {
         m = MatrixUtils.createRealMatrix(input);
     }
 
-    public Matrix(double[] input) {
-        m = MatrixUtils.createColumnRealMatrix(input);
+    public Matrix(double[] input)
+    {
+        m = MatrixUtils.createRowRealMatrix(input);
     }
 
-    public int getNumOfRow() {
+    public int getNumOfRow()
+    {
         return m.getRowDimension();
     }
 
-    public int getNumOfColumn() {
+    public int getNumOfColumn()
+    {
         return m.getColumnDimension();
     }
 
-    public double showsComponentOf(int rowIndex, int columnIndex) {
-        if ((rowIndex > m.getRowDimension()) || (columnIndex > m.getColumnDimension())) {
+    public double showsComponentOf(int rowIndex, int columnIndex)
+    {
+        if ((rowIndex > m.getRowDimension()) || (columnIndex > m.getColumnDimension()))
+        {
             System.out.println("Ordered component does not exist.");
             System.exit(0);
         }
         return m.getEntry(rowIndex, columnIndex);
     }
 
-    public void display(RealMatrix m, String name) {
-        System.out.println(new StringBuilder().append("~~~\\t~~~\\t").append(name).toString());
-        for (int i = 0; i < m.getRowDimension(); i++) {
+    public void display(String name)
+    {
+        System.out.println(new StringBuilder().append("~~~\t~~~\t").append(name));
+        for (int i = 0; i < this.m.getRowDimension(); i++)
+        {
             System.out.print("[");
-            for (int j = 0; j < m.getColumnDimension(); j++) {
-                System.out.print(m.getEntry(i, j));
-                if (j < m.getColumnDimension() - 1) System.out.print(",");
+            for (int j = 0; j < this.m.getColumnDimension(); j++)
+            {
+                System.out.print(this.m.getEntry(i, j));
+                if (j < this.m.getColumnDimension() - 1) System.out.print(",");
             }
             System.out.println("]");
         }
@@ -50,17 +61,21 @@ public class Matrix {
     }
 
     // ベクトルAとBの内積 A・Bの結果を返す
-    public double getInnerProduct(RealMatrix mA, RealMatrix mB) {
+    public double getInnerProduct(Matrix matrix)
+    {
         double result = 0;
-        if (mA.getColumnDimension() == 1) {
+        if (this.m.getColumnDimension() == 1)
+        {
             System.out.println("Matrix column is not enough to solve.");
             System.exit(0);
-        } else if (mA.getRowDimension() == 1) {
-            if (mB.getRowDimension() == 1)
-                result = mA.getRowVector(0).dotProduct(mB.getRowVector(0));
-            else if (mB.getColumnDimension() == 1)
-                result = mA.getRowVector(0).dotProduct(mB.getColumnVector(0));
-            else {
+        }
+        else if (this.m.getRowDimension() == 1)
+        {
+            if (matrix.m.getRowDimension() == 1) result = this.m.getRowVector(0).dotProduct(matrix.m.getRowVector(0));
+            else if (matrix.m.getColumnDimension() == 1)
+                result = this.m.getRowVector(0).dotProduct(matrix.m.getColumnVector(0));
+            else
+            {
                 System.out.println("Matrix dimension is not even.");
                 System.exit(0);
             }
@@ -69,23 +84,27 @@ public class Matrix {
     }
 
     // 行列同士、もしくは行列とベクトルとの積を計算する
-    public Matrix multiplyMatrix(Matrix target) {
-        Matrix ans = null;
+    public Matrix multiplyMatrix(Matrix target)
+    {
+        Matrix ans = new Matrix();
         // 掛けられる行列の列数と掛ける行列の行数が等しいなら
-        if (this.m == target.m) {
+        if (this.m.getColumnDimension() == target.m.getRowDimension())
             ans.m = this.m.multiply(target.m);
-        } else {
+        else
+        {
             System.out.println("Bad combination matrix.");
             System.exit(0);
         }
         return ans;
     }
 
-    public boolean multipliable(Matrix y) {
-        if (this.m.getColumnDimension() == y.m.getRowDimension()) {
-            return true;
-        } else {
+    public boolean isMultipliable(Matrix y)
+    {
+        if (this.m.getColumnDimension() != y.m.getRowDimension())
+        {
+            System.out.println("Bad combination matrix.");
             return false;
         }
+        return true;
     }
 }
