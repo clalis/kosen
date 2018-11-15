27793cfc12ed418cc3c1b33dff17dc8412d99c8b
s_clalis <s_clalis@yahoo.co.jp>
2018-11-15 14:50:42 +0900

    Edited somedsp1-7
    うーん土日虚無ですな。

--- C++/DSP/dsp1-7/dsp1-7/pch.h
+++ C++/DSP/dsp1-7/dsp1-7/pch.h
@@ -5,10 +5,15 @@
 #ifndef PCH_H
 #define PCH_H
 
+#define DAT_N 16384
+#define N 128
+
 #include <complex>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <string>
+#include <vector>
 
 #endif //PCH_H
