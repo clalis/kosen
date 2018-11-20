347ea17e1273f18244c32e8684aa115c730299de
s_clalis <s_clalis@yahoo.co.jp>
2018-11-20 20:33:43 +0900

    Completed DSP1-7
    Fck u.
    http://vivi.dyndns.org/tech/cpp/timeMeasurement.html
    https://www.mk-mode.com/octopress/2013/06/10/cpp-discrete-fourier-transform/
    http://www.densikairo.com/Development/Public/study_dsp/A5D5A1BCA5EAA5A8CAD1B4B940B9E2C2AEA5D5A1BCA5EAA5A8CAD1B4B9.html
    https://cpprefjp.github.io/reference/complex/complex.html

--- C++/DSP/dsp1-7/dsp1-7/dsp1-7/dsp1-7.cpp
+++ C++/DSP/dsp1-7/dsp1-7/dsp1-7/dsp1-7.cpp
@@ -6,17 +6,12 @@
 
 using namespace std;
 
-/*
- * 計算クラス
- */
-class Calc
+// 計算クラス
+class DFT
 {
-	double SRC_re[DAT_N];   //元データの実部
-	double SRC_im[DAT_N];   //元データの虚部
-	double DFT_re[N];   //DFTの実部
-	double DFT_im[N];   //DFTの虚部
-	double IDFT_re[N];  //IDFTの実部
-	double IDFT_im[N];  //IDFTの虚部
+	complex<double> SRC[DAT_N];   //元データの実部
+	complex<double> DFT_dat[N];   //DFT
+	complex<double> IDFT_dat[N];  //IDFT
 
 public:
 	void makeSourceData();  // 元データ作成
@@ -27,91 +22,11 @@ private:
 	double calcTerm(int n, double x);  //各項計算
 };
 
-static const string dftFilename = "DFT.csv";
-static const string idftFilename = "IDFT.csv";
-static const double pi = acos(-1.0); // お手軽π
-
-/*
- * 元データ作成
- */
-void Calc::makeSourceData()
-{
-	int i;
-
-	for (i = 0; i < DAT_N; i++)
-	{
-		SRC_re[i] = 2 * sin(4 * (2 * pi / DAT_N) * i) + 3 * cos(2 * (2 * pi / DAT_N) * i);
-		SRC_im[i] = 0.0;
-	}
-}
-
-/*
- * 離散フーリエ変換
- */
-void Calc::executeDFT()
-{
-	int k, n;  // LOOPインデックス
-
-	// 出力ファイルOPEN
-	ofstream pf(dftFilename, ios::out | ios::trunc);
-
-	// ヘッダ出力 ( k, 角周波数, 元データ(実部), 元データ(虚部), DFT(実部), DFT(虚部) )
-	pf << "k,f,x_re,x_im,X_re,X_im" << endl;
-
-	// 計算・結果出力
-	for (k = 0; k < N; k++)
-	{
-		DFT_re[k] = 0.0;
-		DFT_im[k] = 0.0;
-		for (n = 0; n < N; n++)
-		{
-			DFT_re[k] += SRC_re[n] * (cos((2 * pi / N) * k * n)) + SRC_im[n] * (sin((2 * pi / N) * k * n));
-			DFT_im[k] += SRC_re[n] * (-sin((2 * pi / N) * k * n)) + SRC_im[n] * (cos((2 * pi / N) * k * n));
-		}
-		pf << k << "," << (2 * pi / N) * k << "," << SRC_re[k] << "," << SRC_im[k] << "," << DFT_re[k] << "," << DFT_im[k] << endl;
-	}
-
-	// 出力ファイルCLOSE
-	pf.close();
-}
-
-/*
- * 逆離散フーリエ変換
- */
-void Calc::executeIDFT()
-{
-	int k, n;  // LOOPインデックス
-
-	// 出力ファイルOPEN
-	ofstream pf(idftFilename, ios::out | ios::trunc);
-
-	// ヘッダ出力 ( k, 角周波数, DFT(実部), DFT(虚部), IDFT(実部), IDFT(虚部) )
-	pf << "k,\tf,\tX_re,\tX_im,\tx_re,\tx_im\n" << endl;
-
-	// 計算・結果出力
-	for (n = 0; n < N; n++)
-	{
-		IDFT_re[n] = 0.0;
-		IDFT_im[n] = 0.0;
-		for (k = 0; k < N; k++)
-		{
-			IDFT_re[n] += DFT_re[k] * (cos((2 * pi / N) * k * n)) - DFT_im[k] * (sin((2 * pi / N) * k * n));
-			IDFT_im[n] += DFT_re[k] * (sin((2 * pi / N) * k * n)) + DFT_im[k] * (cos((2 * pi / N) * k * n));
-		}
-		IDFT_re[n] /= N;
-		IDFT_im[n] /= N;
-		pf << "," << n << "," << (2 * pi / N) * n << "," << DFT_re[n] << "," << DFT_im[n] << "," << IDFT_re[n] << "," << IDFT_im[n] << endl;
-	}
-
-	// 出力ファイルCLOSE
-	pf.close();
-}
-
-class fft
+class FFT
 {
 private:
 	complex<long double> w[512];
-	long	br[512];
+	long br[512];
 	long n;
 	long double step;
 	long double pi;
@@ -119,11 +34,71 @@ private:
 	complex<long double> multiplication(complex<long double> a, complex<long double> b);
 
 public:
-	fft(long num);
+	FFT(long num);
 	void difFFT(complex<long double> x[]);
 };
 
-fft::fft(long num)
+static const string dftFilename = "DFT.csv";
+static const string idftFilename = "IDFT.csv";
+static const string fftFilename = "FFT.csv";
+static const string ifftFilename = "IFFT.csv";
+static const double pi = acos(-1.0); // お手軽π
+
+// 元データ作成
+void DFT::makeSourceData()
+{
+	int i;
+
+	for (i = 0; i < DAT_N; i++)	SRC[i] = complex<double>(2 * sin(1024 * (2 * pi / DAT_N) * i) + 3 * cos(256 * (2 * pi / DAT_N) * i));
+}
+
+// 離散フーリエ変換
+void DFT::executeDFT()
+{
+	// 出力ファイルOPEN
+	ofstream pf(dftFilename, ios::out | ios::trunc);
+
+	// ヘッダ出力 ( k, 角周波数, 元データ(実部), 元データ(虚部), DFT(実部), DFT(虚部) )
+	pf << "k,f,x_re,x_im,X_re,X_im,|X(k)|" << endl;
+
+	// 計算・結果出力
+	for (size_t k = 0; k < N; k++)
+	{
+		DFT_dat[k] = complex<double>(0.0, 0.0);
+		for (size_t n = 0; n < N; n++)	DFT_dat[k] += complex<double>(SRC[n].real() * (cos((2 * pi / N) * k * n)) + SRC[n].imag() * (sin((2 * pi / N) * k * n)), SRC[n].real() * (-sin((2 * pi / N) * k * n)) + SRC[n].imag() * (cos((2 * pi / N) * k * n)));
+
+		long double spt = sqrt(DFT_dat[k].real() * DFT_dat[k].real() + DFT_dat[k].imag() * DFT_dat[k].imag());
+		pf << k << "," << (2 * pi / N) * k << "," << SRC[k].real() << "," << SRC[k].imag() << "," << DFT_dat[k].real() << "," << DFT_dat[k].imag() << "," << spt << endl;
+	}
+
+	// 出力ファイルCLOSE
+	pf.close();
+}
+
+// 逆離散フーリエ変換
+void DFT::executeIDFT()
+{
+	// 出力ファイルOPEN
+	ofstream pf(idftFilename, ios::out | ios::trunc);
+
+	// ヘッダ出力 ( k, 角周波数, DFT(実部), DFT(虚部), IDFT(実部), IDFT(虚部) )
+	pf << "k,\tf,\tX_re,\tX_im,\tx_re,\tx_im\n" << endl;
+
+	// 計算・結果出力
+	for (size_t n = 0; n < N; n++)
+	{
+		IDFT_dat[n] = complex<double>(0.0, 0.0);
+		for (size_t k = 0; k < N; k++)	IDFT_dat[n] += complex<double>(DFT_dat[k].real() * (cos((2 * pi / N) * k * n)) - DFT_dat[k].imag() * (sin((2 * pi / N) * k * n)), DFT_dat[k].real() * (sin((2 * pi / N) * k * n)) + DFT_dat[k].imag() * (cos((2 * pi / N) * k * n)));
+		IDFT_dat[n] /= complex<double>(N, N);
+
+		pf << "," << n << "," << (2 * pi / N) * n << "," << DFT_dat[n].real() << "," << DFT_dat[n].imag() << "," << IDFT_dat[n].real() << "," << IDFT_dat[n].imag() << endl;
+	}
+
+	// 出力ファイルCLOSE
+	pf.close();
+}
+
+FFT::FFT(long num)
 {
 	n = num;
 	pi = acos(-1.0);
@@ -142,11 +117,12 @@ fft::fft(long num)
 	}
 }
 
-void fft::difFFT(complex<long double> x[])
+void FFT::difFFT(complex<long double> x[])
 {
-	complex<long double> out[2];							// 一時記憶メモリ
+	complex<long double> out[2];			// 一時記憶メモリ
 	long roop2 = n / 2;
 	long roop = 1;
+
 	for (long i = 0; i < step; i++)			// 第i番目のバタフライ区間
 	{
 		long bias = 0;
@@ -156,7 +132,7 @@ void fft::difFFT(complex<long double> x[])
 			{
 				long num = bias + k;		// バタフライ演算対象信号の選択
 				long num2 = num + roop2;	// バタフライ演算対象信号の選択
-				butterfly(x[num], x[num2], out, k*roop);
+				butterfly(x[num], x[num2], out, k * roop);
 				x[num] = out[0];			// インプレイス
 				x[num2] = out[1];			// インプレイス
 			}
@@ -167,45 +143,45 @@ void fft::difFFT(complex<long double> x[])
 	}
 
 	complex<long double> buffer;							// ベクトルの要素の入れ替え
-	for (long i = 0; i < n; i++)
+	for (long cnt = 0; cnt < n; cnt++)
 	{
-		if (br[i] > i)
+		if (br[cnt] > cnt)
 		{
-			buffer = x[br[i]];
-			x[br[i]] = x[i];
-			x[i] = buffer;
+			buffer = x[br[cnt]];
+			x[br[cnt]] = x[cnt];
+			x[cnt] = buffer;
 		}
 	}
 }
 
-void fft::butterfly(complex<long double> a, complex<long double> b, complex<long double> out[], long k)
+void FFT::butterfly(complex<long double> a, complex<long double> b, complex<long double> out[], long k)
 {
 	complex<long double> d;
+
 	out[0] = complex<long double>(a.real() + b.real(), a.imag() + b.imag());
-
 	d = complex<long double>(a.real() - b.real(), a.imag() - b.imag());
-
-	out[1] = multiplication(d, w[k]);			// 複素数の積
+	out[1] = multiplication(d, w[k]);	// 複素数の積
 }
 
-complex<long double> fft::multiplication(complex<long double> a, complex<long double> b)
+complex<long double> FFT::multiplication(complex<long double> a, complex<long double> b)
 {
-	complex<long double> c;									// 変数 c を 64 ビットの複素数型で宣言
+	complex<long double> c;				// 変数 c を 64 ビットの複素数型で宣言
 
 	c = complex<long double>(a.real() * b.real() - a.imag() * b.imag(), a.real() * b.imag() + a.imag() * b.real());	// 複素数 a と b の乗算 
 
 	return c;	// c を返す。
 }
 
-long main(long argc, char* argv[])
+int main(int argc, char* argv[])
 {
-	long n;
+	int n;
 	long double ts;
-	complex<long double> sf[16384] = { 0.0 };		// 配列 sf[4] を 64 ビットの複素数型で宣言して，"0" に初期化
+	complex<long double> sf[DAT_N];		// 配列 sf[4] を 64 ビットの複素数型で宣言して，"0" に初期化
 	char buf[512];
 
 	cout << "DSP1-7 4J23 H.Shiina" << endl;
-	cout << "After emitted \"data.txt\", u can get \"one_sec.wav\"." << endl;
+	cout << "Enter some ts and n." << endl;
+	cout << "U can get result of." << endl;
 
 	/*
 		complex<float> c(1.0f, 2.0f);
@@ -234,17 +210,25 @@ long main(long argc, char* argv[])
 
 	try
 	{
+		cout << "Starting DFT..." << endl;
+
+		auto start = chrono::system_clock::now();
+
 		// 計算クラスインスタンス化
-		Calc objCalc;
+		DFT objDFT;
 
 		// 元データ作成
-		objCalc.makeSourceData();
+		objDFT.makeSourceData();
 
 		// 離散フーリエ変換
-		// objCalc.executeDFT();
+		objDFT.executeDFT();
+
+		auto msec = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now() - start).count();
+		cout << msec << endl;
 
 		// 逆離散フーリエ変換
-		// objCalc.executeIDFT();
+		objDFT.executeIDFT();
+		cout << "Finished DFT..." << endl;
 	}
 	catch (...)
 	{
@@ -252,33 +236,46 @@ long main(long argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	long double pi = acos(-1.0);	// 円周率の算出
-	printf(" Ts [second] = ");		// サンプリング周期の入力
+	cout << " Ts [second] = ";	// サンプリング周期の入力
 	fgets(buf, sizeof(buf), stdin);
 	sscanf(buf, "%lf", &ts);
-	printf(" N = ");				// 分割数の入力
+	cout << "[N = 2^n] n = ";	// 分割数の入力
 	fgets(buf, sizeof(buf), stdin);
 	sscanf(buf, "%d", &n);
+	n = pow(2.0, n);
 
 	try
 	{
-		cout << "starting fft..." << endl;
-		fft ft(n);
+		cout << "Starting FFT..." << endl;
 
-		long double f0 = 1.0 / ((long double)n * ts);													// 基本周波数の計算
+		// 出力ファイルOPEN
+		ofstream pf(fftFilename, ios::out | ios::trunc);
 
-		for (long i = 0; i < n; i++)	sf[i] = complex<long double>(1.0 + sin(2.0 * pi * 250.0 * (long double)i * ts));	// 離散時間信号
+		FFT ft(n);
+
+		long double f0 = 2 * pi / ((long double)n);															// 基本周波数の計算
+
+		for (size_t i = 0; i < DAT_N; i++)	sf[i] = complex<long double>(2 * sin(1024 * (2 * pi / DAT_N) * i) + 3 * cos(256 * (2 * pi / DAT_N) * i));	// 離散時間信号
+		// 予備データ	1.0 + sin(2.0 * pi * 250.0 * (long double)i * ts)
+
+		auto start = chrono::system_clock::now();
 
 		ft.difFFT(sf);
 
-		printf("\n");
-		printf("f [Hz] FR(kf0) FI(kf0) |F(kf0)|\n");
+		auto msec = chrono::duration_cast<chrono::microseconds>(chrono::system_clock::now() - start).count();
+		cout << msec << endl;
+
+		pf << "f [Hz], FR(kf0), FI(kf0), |F(kf0)|" << endl;
 		for (long i = 0; i < n; i++)
 		{
 			long double spt = sqrt(sf[i].real() * sf[i].real() + sf[i].imag() * sf[i].imag());
-			printf("%6.1f%8.2f%8.2f%9.2f\n", f0 * (long double)i, sf[i].real(), sf[i].imag(), spt);	// 結果表示
+			pf << f0 * (long double)i << "," << sf[i].real() << "," << sf[i].imag() << "," << spt << endl;	// 結果表示
 		}
-		cout << "finished fft..." << endl;
+
+		// 出力ファイルCLOSE
+		pf.close();
+
+		cout << "Finished FFT..." << endl;
 	}
 	catch (...)
 	{
