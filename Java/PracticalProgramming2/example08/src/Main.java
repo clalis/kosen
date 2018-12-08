e0812bd088ab9bb3680101a814b08f60740c9b7f
s_clalis <s_clalis@yahoo.co.jp>
2018-12-08 15:11:43 +0900

    Completed pp2_ex8
    終わったけどアプレットの代替課題は未定だそうで。

--- Java/PracticalProgramming2/example08/src/Main.java
+++ Java/PracticalProgramming2/example08/src/Main.java
@@ -6,8 +6,25 @@ class Main {
     public static void main(String[] args) {
         NonlinearEquation eqn = new NonlinearEquation();
 
-        // 初期反復解を設定
-        // 近似解が得られたら近似解＆何回目の繰り返しで解が得られたかを表示
-        // 得られなければ、解が見つからなかったことを表示
+        if (eqn._solveNLEByLinearIteration(4.0d))
+            System.out.println("X = " + eqn.answer_ + " at iteration " + eqn.iteration_ + ".");
+        else {
+            System.out.println("Cannot solve NLE.");
+            System.exit(0);
+        }
     }
 }
+
+
+/*
+"C:\Program Files\Java\jdk-11.0.1\bin\java.exe" -agentlib:jdwp=transport=dt_socket,address=127.0.0.1:62453,suspend=y,server=n -javaagent:C:\Users\s_cla\.IntelliJIdea2018.2\system\captureAgent\debugger-agent.jar=file:/C:/Users/s_cla/AppData/Local/Temp/capture.props -Dfile.encoding=UTF-8 -classpath "C:\Users\s_cla\Documents\repos\kosen\Java\PracticalProgramming2\example08\out\production\example08;C:\Users\s_cla\.m2\repository\org\apache\commons\commons-math3\3.6.1\commons-math3-3.6.1.jar;C:\Program Files\JetBrains\IntelliJ IDEA 2018.2.5\lib\idea_rt.jar" Main
+        Connected to the target VM, address: '127.0.0.1:62453', transport: 'socket'
+        value = 4.123105625617661, pastValue = 4.0
+        value = 4.138007446297996, pastValue = 4.123105625617661
+        value = 4.139807658128333, pastValue = 4.138007446297996
+        value = 4.140025079408135, pastValue = 4.139807658128333
+        Disconnected from the target VM, address: '127.0.0.1:62453', transport: 'socket'
+        X = 4.140025079408135 at iteration 4.
+
+        Process finished with exit code 0
+*/
--- Java/PracticalProgramming2/example08/src/pr2NLcalc/NonlinearEquation.java
+++ Java/PracticalProgramming2/example08/src/pr2NLcalc/NonlinearEquation.java
@@ -1,26 +1,33 @@
 package pr2NLcalc;
 
 public class NonlinearEquation {
-    public static final double EPSILON = 0.001;
-    public static final int MAXIMUM_IT = 100;
+    private static final double EPSILON = 0.001;
+    private static final int MAXIMUM_IT = 100;
 
-    private double initialValue_;
-    private double answer_;
-    private int iteration_;
+    public double answer_;
+    public int iteration_;
 
-    // コンストラクタ(最低一つ用意せよ）
-    public NonlinearEquation(引数は各自で決定){
-        // 処理を実装せよ
+    public NonlinearEquation() {
+        answer_ = 0.0d;
+        iteration_ = 0;
     }
 
-    private 戻り値 _solveNLEByLinearIteration(引数は各自で決定)
-    {
-        double value,      // x_k に対応
-                pastValue;  // x_{k-1} に対応（初回のpastValue = x_0とする）
+    public Boolean _solveNLEByLinearIteration(double x0) {
+        double value, pastValue;
 
-        // 初期反復解をvalueに設定し
-        // |value - pastValue| が EPSILON 未満となる(近似解が見つかる)、もしくは
-        // 繰り返し回数がMAXIMUM_IT 回に到達するまで繰り返し
-        // 繰り返しで得られる反復解の途中経過を表示するようにすること
+        value = x0;
+
+        for (int i = 0; i < MAXIMUM_IT; i++) {
+            iteration_++;
+            pastValue = value;
+            value = Math.sqrt(10.0d + 3.0d + pastValue);
+            System.out.println("value = " + value + ", pastValue = " + pastValue);
+            if ((value - pastValue) < EPSILON) {
+                answer_ = value;
+                return Boolean.TRUE;
+            }
+        }
+
+        return Boolean.FALSE;
     }
 }
