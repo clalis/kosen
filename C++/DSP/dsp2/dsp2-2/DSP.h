e918981444bb61b822754eed36c7d50dc5ff8aca
s_clalis <s_clalis@yahoo.co.jp>
2019-06-05 19:05:53 +0900

    Completed dsp2-2-1 and dsp2-2-2
    Eigenの追加をcommitするのにてこずりまくった。

---
+++ C++/DSP/dsp2/dsp2-2/DSP.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include "Eigen/Dense"
+
+using namespace Eigen;
+using namespace std;
+
+class DCT
+{
+public:
+	DCT(unsigned int size) :N(size)
+	{
+		cout << "Starting DCT..." << endl;
+	}
+
+	~DCT()
+	{
+		cout << "\nClosing DCT..." << endl;
+	}
+
+	MatrixXf cnk();
+
+private:
+	unsigned int N;
+};
+
+MatrixXf DCT::cnk()
+{
+	MatrixXf m = MatrixXf::Zero(N, N);
+
+	for (size_t n = 0; n < N; n++)
+		for (size_t k = 0; k < N; k++)
+		{
+			if (k == 0)
+				m(k, n) = 1 / sqrtf(static_cast<float>(N));
+			else
+				m(k, n) = M_SQRT2 / sqrtf(static_cast<float>(N)) * cosf((2 * n + 1) * k * M_PI_2 / N);
+		}
+
+	return m;
+}
---
+++ C++/DSP/dsp2/dsp2-2/dsp2-2.cpp
@@ -0,0 +1,47 @@
+﻿// dsp2-2.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+// R01	23_Shīna	DSP2-2-2
+
+#include "DSP.h"
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+	const static Vector4f oned(1, 2, 3, 4);
+	static Matrix4f twod;
+	twod <<
+		1, 1, 1, 1,
+		2, 1, 2, 1,
+		3, 1, 3, 1,
+		4, 1, 4, 1;
+
+	Vector4f onedDCT = Vector4f::Zero(), onedIDCT = Vector4f::Zero();
+	Matrix4f twodDCT = Matrix4f::Zero();
+	DCT dct(static_cast<int>(oned.size()));
+
+	cout << "R01\t23_Shīna\tDSP2-2-2\n\n";
+
+	onedDCT = dct.cnk() * oned;
+	onedIDCT = dct.cnk().transpose() * onedDCT;
+
+	cout << "DCT :\n" << onedDCT << endl;
+	cout << "IDCT :\n" << onedIDCT << endl;
+
+	twodDCT = dct.cnk() * twod * dct.cnk().transpose();
+
+	cout << "DCT :\n" << twodDCT << endl;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
