f181a294319664448b9ad8060cbac230b0680b0d
s_clalis <s_clalis@yahoo.co.jp>
2019-06-29 16:01:47 +0900

    Completed dsp2-3-1 and dsp2-3-2
    両方終わって提出した。
    入試直前にやめてくれ()

---
+++ C++/DSP/dsp2/dsp2-3/Jacobi.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include<cmath>
+#include<iostream>
+#include"../Eigen/Core"
+
+
+using namespace std;
+using namespace Eigen;
+
+class Jacobi
+{
+public:
+	Jacobi();
+	~Jacobi();
+	void calc_jacobi();
+
+private:
+	Matrix3d a;
+	Vector3d x0;
+	Vector3d x1;
+	const double epsilon = 0.0001;
+	double lambda;
+};
+
+inline Jacobi::Jacobi()
+{
+	a <<
+		2, 1, 3,
+		1, 2, 3,
+		3, 3, 20;
+	x0 = Vector3d::UnitX();
+	x1 = Vector3d::Zero();
+	lambda = 0;
+}
+
+inline Jacobi::~Jacobi()
+{
+}
+
+inline void Jacobi::calc_jacobi()
+{
+	while (true)
+	{
+		bool finish = true;
+
+		x1 = a * x0;
+		lambda = x1.transpose() * x0;
+		x1.normalize();
+		if ((x1 - x0).norm() > epsilon)	finish = false;
+		x0 = x1;
+
+		cout << "lambda= " << lambda << endl << "Eigenvecor:" << endl << x1 << endl << endl;
+		if (finish) return;
+	}
+}
